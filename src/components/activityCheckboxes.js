36694815f76277caaebc120cb6c1db1155e94daa
ashley potts <ashley.potts@asurion.com>
2023-07-10 16:23:56 -0500

    Fixed checkbox error

--- src/components/activityCheckboxes.js
+++ src/components/activityCheckboxes.js
@@ -1,120 +1,111 @@
-import Check from './checkbox';
+
+
+// // function ApiPost(){
+// //     ApiPostStatus=!ApiPostStatus
+// //     let styleValue="display:none"
+// //     if(ApiPostStatus)
+// //         {styleValue="display:block"}
+// //     return(
+// //     <form id="ApiPostform" style={styleValue}>
+// //         <Check title="enabled"/>
+// //         <Text title="connectionId"/>
+// //         <Text title="endpoint"/>
+// //         <Text title="path"/>
+// //         <Text title="headers"/>
+// //         <Text title="payload"/>
+// //         <Text title="blobpath"/>
+// //         <Num title="statusCode"/>
+// //         <Text title="message"/>
+// //         <Check title="raiseException"/>
+// //     </form>
+
+
+
+
+
+
+import React, { useState } from 'react';
 import Text from './textbox';
-import Num from './number';
-import Radio from './radio';
-import { useState } from 'react';
 
 const activities = [
-    {title: "ApiPost"},
-    {title: "BigPandaAlert"},
-    {title: "DatabaseLookup"},
-    {title: "DbStaging"},
-    {title: "EventGridPost"},
-    {title: "ExecuteSql"},
-    {title: "JsonFlatten"},
-    {title: "JsonMap"},
-    {title: "LoadCsv"},
-    {title: "LoadCsvBulk"},
-    {title: "LoadExcel"},
-    {title: "Log"},
-    {title: "SerialNumberConversion"},
-    {title: "Sleep"},
-    {title: "SnsPost"},
-    {title: "WriteFile"},
-    {title: "WriteToStagingDb"}
-    ]
+  { title: "ApiPost" },
+  { title: "BigPandaAlert" },
+  { title: "DatabaseLookup" },
+  { title: "DbStaging" },
+  { title: "EventGridPost" },
+  { title: "ExecuteSql" },
+  { title: "JsonFlatten" },
+  { title: "JsonMap" },
+  { title: "LoadCsv" },
+  { title: "LoadCsvBulk" },
+  { title: "LoadExcel" },
+  { title: "Log" },
+  { title: "SerialNumberConversion" },
+  { title: "Sleep" },
+  { title: "SnsPost" },
+  { title: "WriteFile" },
+  { title: "WriteToStagingDb" }
+];
 
-        let ApiPostStatus=false
-        let BigPandaAlertStatus=false
-        let DatabaseLookupStatus=false
-        let DbStagingStatus=false
-        let EventGridPostStatus=false
-        let ExecuteSqlStatus=false
-        let JsonFlattenStatus=false
-        let JsonMapStatus=false
-        let LoadCsvStatus=false
-        let LoadCsvBulkStatus=false
-        let LoadExcelStatus=false
-        let LogStatus=false
-        let SerialNumberConversionStatus=false
-        let SleepStatus=false
-        let SnsPostStatus=false
-        let WriteFileStatus=false
-        let WriteToStagingDbStatus=false
-
-function ApiPost(){
-    ApiPostStatus=!ApiPostStatus
-    let styleValue="display:none"
-    if(ApiPostStatus)
-        {styleValue="display:block"}
-    return(
-    <form id="ApiPostform" style={styleValue}>
-        <Check title="enabled"/>
-        <Text title="connectionId"/>
-        <Text title="endpoint"/>
-        <Text title="path"/>
-        <Text title="headers"/>
-        <Text title="payload"/>
-        <Text title="blobpath"/>
-        <Num title="statusCode"/>
-        <Text title="message"/>
-        <Check title="raiseException"/>
-    </form>
-
-    )
+function Checkbox({ title, checked, onChange }) {
+  return (
+    <tr>
+      <td>
+        <input
+          type="checkbox"
+          checked={checked}
+          id={title}
+          name={title}
+          onChange={onChange}
+        /><br></br>
+      </td>
+      <td>
+        <label htmlFor={title}>{title}</label>
+      </td>
+    </tr>
+  );
 }
 
-function Checkbox(props){
-    return(
-        <tr>
-            <td>
-                {/* <input type="checkbox" id={props.title} name={props.title}/> */}
-            <input type="checkbox" id={props.title} name={props.title} onChange={ApiPost}/><br></br>
-            </td>
-            <td>
-                <label for={props.title}>{props.title}</label>
-            </td>
-        </tr>
-    )
-}
+function MakeCheckboxes() {
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
 
-function MakeCheckboxes(){
-    const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
- 
-    const handleChange = (event, activity) => {
-        if(event.target.checked) {
-            setSelectedCheckboxes([...selectedCheckboxes, activity]);
-        }
-        else {
-            setSelectedCheckboxes(selectedCheckboxes.filter(item => item !== activity))
-        }
-    };
+  const handleChange = (event, activity) => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      setSelectedCheckboxes(prevCheckboxes => [...prevCheckboxes, activity.title]);
+    } else {
+      setSelectedCheckboxes(prevCheckboxes => prevCheckboxes.filter(item => item !== activity.title));
+    }
+  };
 
-    return(
-        <div>
-            <table>
-        {activities.map(activity=> (
-            <div key={activity.title}>
+  return (
+    <div>
+      <table>
+        <tbody>
+          {activities.map(activity => (
+            <React.Fragment key={activity.title}>
+              <tr>
+                <td>
+                  <Checkbox
+                    title={activity.title}
+                    checked={selectedCheckboxes.includes(activity.title)}
+                    onChange={event => handleChange(event, activity)}
+                  />
+                </td>
+              </tr>
+              {selectedCheckboxes.includes(activity.title) && (
                 <tr>
-                    <td>
-                    <input type="checkbox" checked={selectedCheckboxes.includes(activity)} id={activity.title} name={activity.title} onChange={event => handleChange(event, activity.title)}/>
-                    </td>
-                    <td>
-                        <label for={activity.title}>{activity.title}</label>
-                    </td>
+                  <td colSpan="2">
+                    <Text title="test" />
+                  </td>
                 </tr>
-                <tr>
-                    <td colSpan="2">
-                {selectedCheckboxes.includes(activity.title) && <Text title="test"/>}
-                    </td>
-                </tr>
-                </div>
-        )
-        )
-        }
-        </table>
-        </div> 
-)
+              )}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default MakeCheckboxes;
